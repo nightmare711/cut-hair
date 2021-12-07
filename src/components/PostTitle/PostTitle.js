560627663ef9bddd163135b0dd08182c263d7acc
danhpq176 <danhpq176@github.com>
2021-12-07 10:04:50 +0700

    update ref

--- src/components/PostTitle/PostTitle.js
+++ src/components/PostTitle/PostTitle.js
@@ -12,7 +12,7 @@ export const PostTitle = ({ type }) => {
 					{listServices.map((post) => (
 						<li
 							onClick={() =>
-								window.open(`https://www.trungtambaohanh-nguyenkim.com/post/${post.href}`, '_self')
+								window.open(`https://www.dichvusuachua-nguyenkim.com/post/${post.href}`, '_self')
 							}
 							className='content'
 						>
@@ -30,10 +30,7 @@ export const PostTitle = ({ type }) => {
 				{listPost.map((post) => (
 					<li
 						onClick={() =>
-							window.open(
-								`https://www.trungtambaohanh-nguyenkim.com/services/${post.href}`,
-								'_self'
-							)
+							window.open(`https://www.dichvusuachua-nguyenkim.com/services/${post.href}`, '_self')
 						}
 						className='content'
 					>
